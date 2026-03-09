0a2d43617d08e66f109979621ef91423cc13666a
Adrián Faz <51978727+fazfaz7@users.noreply.github.com>
2026-03-09 10:40:47 +0100

    Initial Commit

---
+++ ManoMath/ManoMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManoMathApp.swift
+//  ManoMath
+//
+//  Created by Adrian Emmanuel Faz Mercado on 09/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ManoMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
